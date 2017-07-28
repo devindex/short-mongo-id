9b93699cb60a434a0e71bdb94f8839de7898ce97
Sergio Rodrigues <sergio@devindex.com.br>
2017-07-28 11:25:44 -0300

    Release script

---
+++ release.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Releasing $VERSION ..."
+
+  # commit
+  npm version $VERSION --message "[release] $VERSION"
+
+  # publish
+  git push origin refs/tags/v$VERSION
+  git push
+  npm publish
+fi
